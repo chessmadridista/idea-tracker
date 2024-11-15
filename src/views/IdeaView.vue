7c40377a30249097b14ba0eaa610b8bc66d75c6c
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-11-15 19:08:58 +0530

    feat: add edit features dialog in the idea view.

--- src/views/IdeaView.vue
+++ src/views/IdeaView.vue
@@ -3,10 +3,12 @@ import IdeaCardDetails from '@/components/idea/IdeaCardDetails.vue';
 import IdeaDialogDeleteIdea from '@/components/idea/IdeaDialogDeleteIdea.vue';
 import IdeaDialogEditIdea from '@/components/idea/IdeaDialogEditIdea.vue';
 import FeaturesCard from '@/components/features/FeaturesCard.vue';
+import FeaturesDialogEditFeature from '@/components/features/FeaturesDialogEditFeature.vue';
 </script>
 <template>
     <IdeaCardDetails />
     <IdeaDialogEditIdea />
     <IdeaDialogDeleteIdea />
     <FeaturesCard />
+    <FeaturesDialogEditFeature />
 </template>
