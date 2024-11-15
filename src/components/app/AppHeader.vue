bfed520475800be018e20e4169d93f6b70f86215
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-11-15 08:40:38 +0530

    feat: add redirection to the home page on header title click.

--- src/components/app/AppHeader.vue
+++ src/components/app/AppHeader.vue
@@ -3,7 +3,16 @@ import AppButtonLogout from './AppButtonLogout.vue'
 </script>
 <template>
     <v-app-bar app color="primary">
-        <v-app-bar-title>Idea Tracker for Indie Hackers</v-app-bar-title>
+        <router-link to="/" class="title-container ml-4">
+            <v-app-bar-title>Idea Tracker for Indie Hackers</v-app-bar-title>
+        </router-link>
+        <v-spacer></v-spacer>
         <AppButtonLogout />
     </v-app-bar>
 </template>
+<style scoped>
+.title-container {
+    text-decoration: none; 
+    color: inherit;
+}
+</style>
