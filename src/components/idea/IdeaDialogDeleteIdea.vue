6a4c437ca60fc532b4409a7717f6f938c7daee36
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-11-16 17:14:32 +0530

    feat: adjust margin.

--- src/components/idea/IdeaDialogDeleteIdea.vue
+++ src/components/idea/IdeaDialogDeleteIdea.vue
@@ -35,13 +35,19 @@ function yes() {
 </script>
 <template>
     <v-dialog v-model="ideaStore.deleteIdeaDialogVisibility">
-        <v-card>
+        <v-card class="pa-4 rounded-xl delete-idea-card">
             <v-card-title class="text-center">Do you want to delete this idea?</v-card-title>
             <v-card-actions>
-                <v-btn color="#007bff" variant="outlined" @click="no">No</v-btn>
+                <v-btn color="#007bff" class="rounded-pill" variant="outlined" @click="no">No</v-btn>
                 <v-spacer></v-spacer>
-                <v-btn color="error" variant="outlined" @click="yes">Yes</v-btn>
+                <v-btn color="error" class="rounded-pill" variant="outlined" @click="yes">Yes</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
 </template>
+<style scoped>
+.delete-idea-card {
+    max-width: 400px;
+    margin: auto;
+}
+</style>
