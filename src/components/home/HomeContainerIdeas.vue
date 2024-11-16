114401d45b853387cc8be80cd7db7a4f238bad0f
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-11-16 15:51:29 +0530

    feat: add border to card.

--- src/components/home/HomeContainerIdeas.vue
+++ src/components/home/HomeContainerIdeas.vue
@@ -22,7 +22,7 @@ onBeforeMount(() => {
     <v-container>
         <v-row>
             <v-col v-for="idea in ideaStore.ideas" :key="idea.id" cols="12" lg="6" xl="4">
-                <v-card class="pa-4" :to="{ name: 'idea', params: { id: idea.id } }">
+                <v-card class="pa-4 rounded-xl elevation-4" :to="{ name: 'idea', params: { id: idea.id } }">
                     <v-card-title class="text-center text-primary-darken-1">
                         {{ idea.name }}
                     </v-card-title>
