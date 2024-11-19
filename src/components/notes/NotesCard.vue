53d785dfa35b446ddfa59607975a270bb285d972
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-11-19 13:27:23 +0530

    feat: add background color and other information for notes.

--- src/components/notes/NotesCard.vue
+++ src/components/notes/NotesCard.vue
@@ -1,35 +1,35 @@
 <script setup>
-import { useJournalStore, useGeneralStore } from '@/stores';
+import { useNoteStore, useGeneralStore } from '@/stores';
 import { onBeforeMount, inject, ref, nextTick } from 'vue';
 import { useRouter } from 'vue-router';
 
 const axios = inject('axios')
-const journalStore = useJournalStore()
+const noteStore = useNoteStore()
 const generalStore = useGeneralStore()
-const newJournalInputFieldVisibility = ref(false)
+const newNoteInputFieldVisibility = ref(false)
 const router = useRouter()
 const form = ref(null)
-const newJournalTextAreaRef = ref(null)
-const newJournal = ref('')
+const newNoteTextAreaRef = ref(null)
+const newNote = ref('')
 
-function addNewJournal() {
+function addNewNote() {
     form.value.validate().then((response) => {
         if (response.valid) {
             const ideaId = router.currentRoute.value.params.id
-            const endPoint = "/create-journal"
+            const endPoint = "/create-note"
             const formData = new FormData()
             formData.append('idea_id', ideaId)
-            formData.append('journal_description', newJournal.value)
+            formData.append('note_description', newNote.value)
             axios.post(endPoint, formData)
             .then(response => {
-                const journal = {
-                    id: response.data.journal_id,
+                const note = {
+                    id: response.data.note_id,
                     idea_id: ideaId,
-                    description: newJournal.value,
+                    description: newNote.value,
                 }
-                journalStore.addJournal(journal)
-                newJournal.value = ''
-                newJournalInputFieldVisibility.value = false
+                noteStore.addNote(note)
+                newNote.value = ''
+                newNoteInputFieldVisibility.value = false
                 generalStore.setSnackbarMessage(response.data.message)
                 generalStore.setSnackbarColor('success')
                 generalStore.showSnackbar()
@@ -40,68 +40,68 @@ function addNewJournal() {
     })
 }
 
-function editJournal(journal) {
-    journalStore.setEditedJournalDescription(journal.description)
-    journalStore.setEditedJournal(journal)
-    journalStore.showEditJournalDialog()
+function editNote(note) {
+    noteStore.setEditedNoteDescription(note.description)
+    noteStore.setEditedNote(note)
+    noteStore.showEditNoteDialog()
 }
 
-function getJournals() {
+function getNotes() {
     const ideaId = router.currentRoute.value.params.id
-    const endPoint = `/get-journals?idea_id=${ideaId}`
+    const endPoint = `/get-notes?idea_id=${ideaId}`
     axios.get(endPoint)
     .then(response => {
-        journalStore.setJournals(response.data.journals)
+        noteStore.setNotes(response.data.notes)
     })
     .catch(error => {
-        journalStore.setJournals(error.response.data.journals)
+        noteStore.setNotes(error.response.data.notes)
     })
 }
 
-function showNewJournalInputField() {
-    newJournalInputFieldVisibility.value = true
+function showNewNoteInputField() {
+    newNoteInputFieldVisibility.value = true
     nextTick(() => {
-        if (newJournalTextAreaRef.value) {
-            newJournalTextAreaRef.value.focus();
+        if (newNoteTextAreaRef.value) {
+            newNoteTextAreaRef.value.focus();
         }
     })
 }
 
 onBeforeMount(() => {
-    getJournals()
+    getNotes()
 })
 </script>
 <template>
     <v-card class="pa-4 rounded-xl">
         <v-card-title class="text-center text-blue-grey-darken-2">
-            Your learnings
+            Your notes
         </v-card-title>
-        <v-card-text v-if="journalStore.journals.length > 0">
+        <v-card-text v-if="noteStore.notes.length > 0">
             <v-list>
-                <v-list-item prepend-icon="mdi-book-outline" class="text-pre-wrap bg-green-lighten-5 mt-2 py-4" rounded="lg" v-for="journal in journalStore.journals" @click="editJournal(journal)" :key="journal.id">
-                    {{ journal.description }}
+                <v-list-item prepend-icon="mdi-note-text-outline" class="text-pre-wrap bg-yellow-lighten-4 mt-2 py-4" rounded="lg" v-for="note in noteStore.notes" @click="editNote(note)" :key="note.id">
+                    {{ note.description }}
                 </v-list-item>
             </v-list>
         </v-card-text>
-        <v-card-text v-if="newJournalInputFieldVisibility">
-            <v-form ref="form" @submit.prevent="addNewJournal">
+        <v-card-text v-if="newNoteInputFieldVisibility">
+            <v-form ref="form" @submit.prevent="addNewNote">
                 <v-textarea 
-                    ref="newJournalTextAreaRef"
+                    ref="newNoteTextAreaRef"
                     color="primary"
-                    label="Describe the journal in detail*"
-                    v-model="newJournal"
+                    label="Describe the note in detail*"
+                    v-model="newNote"
                     :rules="[(v) => !!v || 'This field is required.']"
                 />
                 <div class="text-right">
                     <v-btn class="rounded-pill" type="submit" variant="elevated" color="#28a745" prepend-icon="mdi-check">
-                        Add this journal
+                        Add this note
                     </v-btn>
                 </div>
             </v-form>
         </v-card-text>
         <v-card-actions v-else class="d-flex justify-end">
-            <v-btn variant="elevated" color="#007bff" @click="showNewJournalInputField" prepend-icon="mdi-plus" class="rounded-pill">
-                Create new journal
+            <v-btn variant="elevated" color="#007bff" @click="showNewNoteInputField" prepend-icon="mdi-plus" class="rounded-pill">
+                Create new note
             </v-btn>
         </v-card-actions>
     </v-card>
