09ed764e2704ea444473d0c6466ab3645a0965d0
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-11-19 13:04:15 +0530

    feat: add notes in the view.

--- src/views/IdeaView.vue
+++ src/views/IdeaView.vue
@@ -6,6 +6,8 @@ import FeaturesCard from '@/components/features/FeaturesCard.vue';
 import FeaturesDialogEditFeature from '@/components/features/FeaturesDialogEditFeature.vue';
 import JournalsCard from '@/components/journals/JournalsCard.vue';
 import JournalsDialogEditJournal from '@/components/journals/JournalsDialogEditJournal.vue';
+import NotesCard from '@/components/notes/NotesCard.vue';
+import NotesDialogEditJournal from '@/components/notes/NotesDialogEditJournal.vue';
 </script>
 <template>
     <IdeaCardDetails />
@@ -17,7 +19,16 @@ import JournalsDialogEditJournal from '@/components/journals/JournalsDialogEditJ
                 <FeaturesCard />
             </v-col>
             <v-col cols="12" lg="6">
-                <JournalsCard />
+                <v-row>
+                    <v-col cols="12">
+                        <NotesCard />
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12">
+                        <JournalsCard />
+                    </v-col>
+                </v-row>
             </v-col>
         </v-row>
     </v-container>
